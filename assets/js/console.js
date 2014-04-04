082aeaf518c5bd4a271dabb023b7b7eecf042d07
Michael Levy <mike.levy75@gmail.com>
2014-04-04 16:07:25 -0400

    added min file, cleaned up and added json.stringify

--- assets/js/console.js
+++ assets/js/console.js
@@ -1,52 +1,119 @@
 (function () {	
 	/**
-	* @functions
-	* Some general functions to make coding easier.
-	* These functions include:
-	* -- creating element on the fly (with attributes),
-	* -- get element by id shortcut
-	* -- getting the size of an Object
-	* -- styling an element using an Object
-	* -- device detection
+	* UTILITIES
+	* Functions to make accessing the DOM and other functionality easier.
 	*/
-	var objKeyHTML = '';
-	function create(e,t){var n=document.createElement(e);if(t){var r=t;for(var i in r){[i]=="class"?n.className=r[i]:n.setAttribute([i],r[i])}}return n}
-	function $(e){return document.getElementById(e)}
-	function objSize(e){var t=0;for(var n in e){t++}return t}
-	function styleElement(e,t){var n=t;for(var r in n){e.style[r]=n[r]}}
-	function isMobile(){return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) ? true : false;}
-	function objectKeys(data, num) {
-		for (var key in data) {
-			if (typeof(data[key]) == "object" && data[key] != null) {
-				//alert("Key: " + key + "\nValues: " + data[key]);
-				
-				objKeyHTML += key+': {';
-				objectKeys(data[key]);
-			} else {
-				if (Object.prototype.toString.call(data[key]) === '[object Array]') {
-					//objKeyHTML += data[key]+", ";
-				} else {
-					objKeyHTML += key+": "+data[key]+", ";
-				}
+
+	/**
+	* @function $
+	* Returns an element based on it's id.
+	*
+	* @param {String} id - the name of the id you would like to retrieve.
+	*/
+	function $(id) {
+		return document.getElementById(id);
+	}
+
+	/**
+	* @function create
+	* Returns an element to be created in the DOM and adds attributes.
+	* NOTE: It is best to put it in a variable.
+	* Usage: var test = ML.El.create(element, {'attribute': 'attributeValue'});
+	*
+	* @param {String} tag - tag you want to created, i.e "div", "span", etc...
+	* @param {Object} args - arguments passed.
+	*** @param {Object} attrs - attributes you want on the tag, i.e. class="test", src="img.jpg", etc...
+	*/
+	function create(element, arg) {
+		var elem = document.createElement(element);
+
+		if (arg) {
+			var attrs = arg;
+
+			for (var attr in attrs) {
+				// IE does not support support setting class name with set attribute
+				([attr] == 'class') ? elem.className = attrs[attr] : elem.setAttribute([attr], attrs[attr]);
 			}
 		}
+
+		return elem;
 	}
+
+	/**
+	* @function objSize
+	* Returns the size of an object.
+	*
+	* @param {Object} obj - object to be measured.
+	*/
+	function objSize(obj){
+		var count = 0;
+		
+		for(var prop in obj){
+			count++;
+		}
+		
+		return count;
+	}
+
+	/**
+	* @function styleElement
+	* Adds styles to an element.
+	* Usage: ML.El.styl(element, 'display': 'none', 'overflow': 'hidden'});
+	*
+	* @param {HTMLElement} element - element you want styled.
+	* @param {Object} args - arguments passed.
+	*** @param {Object} arg.props - styles you want applied to the element.
+	*/
+	function styleElement(element, arg) {
+		var props = arg;
+
+		for (var prop in props) {
+			element.style[prop] = props[prop];
+		}
+	}
+
+	/**
+	* @function isMobile
+	* Returns true or false if we are viewing on a mobile or tablet device.
+	*/
+	function isMobile(){
+		return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) ? true : false;
+	}
+
+	// Credits: https://github.com/douglascrockford/JSON-js
+	JSON.stringify = function (obj) {
+		var t = typeof (obj);
+		if (t != "object" || obj === null) {
+			// simple data type
+			if (t == "string") obj = '"'+obj+'"';
+			return String(obj);
+		}
+		else {
+			// recurse array or object
+			var n, v, json = [], arr = (obj && obj.constructor == Array);
+			for (n in obj) {
+				v = obj[n]; t = typeof(v);
+				if (t == "string") v = '"'+v+'"';
+				else if (t == "object" && v !== null) v = JSON.stringify(v);
+				json.push((arr ? "" : '"' + n + '": ') + String(v));
+			}
+			return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
+		}
+	};
 
 	// Start of the custom console.log
 	// If the console is undefined or using a mobile device.
 	if (typeof console == 'undefined' || isMobile()) {
 		var output, 
 			div = create('div', {id: 'consoleLog'}),
-			ul = create('ul'), h6 = create('h6'),
-			count = 0, // Simple counter
-			oldConsole = console; // keep the old console just in case.
-
+			ul = create('ul'), h6 = create('h6');
+			
 		h6.innerHTML = 'CONSOLE LOG';
 		
-		// Styles the individual elements: h6 & ul
-		styleElement(div,{position:"fixed",bottom:"0",left: 0,width:"97%","font-size":"12px",background:"#fff",zIndex:"999999999999999999999"});
-		styleElement(ul,{margin:0,padding:0,overflow:"auto",height:Math.round(window.innerHeight/4.5)+"px"});
-		styleElement(h6,{margin:0,padding:"5px","font-size":"13px","border-bottom":"1px solid #ccc"})
+		// Styles the individual elements: h6, ul and holder of logs.
+		styleElement(div,{position:"fixed",bottom:"0",left: 0,width:"97%","fontSize":"12px",background:"#fff",zIndex:"999999999999999999999","fontFamily": "Arial"});
+		styleElement(ul,{margin:0,padding:0,overflow:"auto",height:Math.round(window.innerHeight/4.5)+"px", "fontFamily":"Times New Roman","fontSize":"12px"});
+		styleElement(h6,{margin:0,padding:"5px","fontSize":"13px","border-bottom":"1px solid #ccc"})
 		
 		div.appendChild(h6);
 		div.appendChild(ul);
@@ -55,38 +122,17 @@
 		window.console = {
 			log: function(){
 				output = ''; // used to clear the output each time
-				count++;
 				
 				for(var i=0; i<arguments.length; i++) {
 					var param = arguments[i],
-						li = create('li'),
-						objCount = 0;
+						li = create('li');
 					
 					// Each log is placed inside an li element.
-					styleElement(li, {'padding': '5px','background': 'white','border-bottom': '1px solid #ccc'});
+					styleElement(li, {'padding': '5px 16px 5px 5px','background': 'white','border-bottom': '1px solid #ccc'});
 
 					// If the parameter is an object special functionality needs to happen.
 					if (typeof param == 'object') {
-						
-						for (var prop in param) {
-							var value = param[prop];
-							objCount++;
-							objKeyHTML = '';
-							
-							if (Object.prototype.toString.call(value) === '[object Array]') {
-								objectKeys(value, objCount);
-								output += prop+': ['+objKeyHTML+'], ';
-							} else if (typeof value == 'object') {
-								objectKeys(value, objCount);
-								output += prop+': {'+objKeyHTML+'}, ';
-							} else {
-								output += prop+': '+value+', '
-							}
-						}
-						
-						output = 'Object {'+output+'}';
-						output = output.replace(/, \}+/g, '}');
-						output = output.replace(/, \]+/g, ']');
+						output = 'Object '+JSON.stringify(param);
 						
 						// Since null keyword is an object
 						if (param == null) {output = 'null';}
@@ -98,11 +144,7 @@
 				li.innerHTML = output;
 				ul.appendChild(li);
 				document.body.appendChild(div);
-			},
-			
-			error: function(){},
-			
-			info: function(){}
+			}
 		}
 	}
 }());
