f732285dba032ce8c6972b060e464d8e60d6747c
Michael Levy <michaellev@lt-mbp-nyc-257.ny.rga.com>
2014-07-02 16:28:59 -0400

    -removed elementbyid -header element to console log script -special if statements in case if window, document or html elements were logged -color to li elements -changing the height of the console via input

--- js/console.js
+++ js/console.js
@@ -5,16 +5,6 @@
 	*/
 
 	/**
-	* @function $
-	* Returns an element based on it's id.
-	*
-	* @param {String} id - the name of the id you would like to retrieve.
-	*/
-	function $(id) {
-		return document.getElementById(id);
-	}
-
-	/**
 	* @function create
 	* Returns an element to be created in the DOM and adds attributes.
 	* NOTE: It is best to put it in a variable.
@@ -91,45 +81,77 @@
 	if (typeof console == 'undefined' || isMobile()) {
 		var output, consoleShown = true, 
 			div = create('div', {id: 'consoleLog'}),
-			ul = create('ul', {id: 'consoleLog-UL'}), h6 = create('h6');
+			header = create('div', {id: 'consoleLog-header'}),
+			ul = create('ul', {id: 'consoleLog-ul'}),
+			input = create('input', {id: 'consoleLog-input', type: 'text', value: Math.round(window.innerHeight/4), maxlength: 3}),
+			oldConsole = console;
 			
-		h6.innerHTML = 'CONSOLE LOG <span style="float:right;color:#ccc;">[click to toggle]</span>';
-		
+		header.innerHTML = '<h6 style="margin:5px 3px;padding:0;float:left;font-size:13px;">CONSOLE LOG</h6>'+
+						   '<a id="consoleLog-toggle" href="javascript:void(0);" style="padding:5px; display:block;font-weight:bold;float:right;color:#ccc;text-decoration:none;font-size:13px;">[click to hide]</a>'+
+						   '<span style="float:right;margin-right:10px;">Height: </span>';
+						   
 		// Styles the individual elements
-		styleElement(div,{margin:0,padding:0,position:"fixed",bottom:"0",left: 0,width:"97%","fontSize":"12px",background:"#fff",zIndex:"999999999999999999999","fontFamily": "Arial", 'border-top':'1px solid #999'});
-		styleElement(ul,{margin:0,padding:0,overflow:"auto",height:Math.round(window.innerHeight/4.5)+"px", "fontFamily":"Times New Roman","fontSize":"12px"});
-		styleElement(h6,{margin:0,padding:"5px","fontSize":"13px","border-bottom":"1px solid #ccc", cursor:'pointer'});
-		
-		div.appendChild(h6);
+		styleElement(div,{margin:0, padding:0,position:"fixed",bottom:"0",left: 0,width:"100%","fontSize":"12px",background:"#fff",zIndex:"999999999999999999999","fontFamily": "Arial", 'border-top':'1px solid #999'});
+		styleElement(ul,{margin:0, padding:0,overflow:"auto",height:Math.round(window.innerHeight/4)+"px", "fontFamily":"Times New Roman","fontSize":"12px", 'color': '#000000'});
+		styleElement(header,{'overflow': 'auto', margin:0, padding:"2px", "border-bottom":"1px solid #ccc", 'color': '#000000'});
+		styleElement(input, {"fontFamily":"Times New Roman","fontSize":"12px", 'color': '#000000', 'width': '25px', 'padding': '2px'})
+
+		div.appendChild(header);
 		div.appendChild(ul);
+		header.getElementsByTagName('span')[0].appendChild(input);
 		
 		// Toggle console
-		h6.onclick = function(e){
-			if (consoleShown) {
-				$('consoleLog-UL').style.display = 'none';
-				consoleShown = false;
-			} else {
-				$('consoleLog-UL').style.display = 'block';
-				consoleShown = true;
+		document.onclick = function(e){
+			var element = e.target || e.srcElement;
+        	if (element.nodeType == 3) element = element.parentNode; // http://www.quirksmode.org/js/events_properties.html
+			
+			if (element.id == 'consoleLog-toggle') {
+				ul.style.display = (consoleShown) ? 'none' : 'block';
+				consoleShown = (consoleShown) ? false : true;
+				element.innerHTML = '[click to '+((!consoleShown) ? 'show' : 'hide')+']';
 			}
+		};
+
+		// Change height 
+		input.onkeyup = function(e) {
+			if (isNaN(this.value) || this.value == '') {
+				this.value = this.defaultValue;
+			}
+
+			ul.style.height = this.value+'px';
 		};
 		
 		// Overwrites the console
 		window.console = {
 			log: function(){
-				output = ''; // used to clear the output each time
+				output = ''; // used to clear the output each time				
+
+
+				// oldConsole.log(arguments.parentNode)
 				
 				for(var i=0; i<arguments.length; i++) {
 					var param = arguments[i],
 						li = create('li');
-					
+
 					// Each log is placed inside an li element.
-					styleElement(li, {'padding': '5px 16px 5px 5px','background': 'white','border-bottom': '1px solid #ccc'});
+					styleElement(li, {'padding': '5px 16px 5px 5px','background': 'white','border-bottom': '1px solid #ccc', 'color': '#000000'});
 
 					// If the parameter is an object special functionality needs to happen.
 					if (typeof param == 'object') {
-						output = 'Object '+JSON.stringify(param);
-						
+						if (param.tagName || param[0] !== undefined || param.window || (param.parentNode == null && param.childNodes)) {
+							var el = (param[0] !== undefined) ? param[0] : param;
+							if (param.window) el.nodeName = 'window';
+
+							output += el.nodeName+'<br />'+
+									  '<em>* For more information, please log a specific aspect of the '+el.nodeName+' element. For example its id, attributes, length, childNodes and/or etc...</em>';
+						} else {
+							if (param.toString() == '[object Object]') {
+								output += '<span style="display:block;">Object '+JSON.stringify(param)+'</span>';
+							} else {
+								output += param;
+							}
+						}
+											
 						// Since null keyword is an object
 						if (param == null) {output = 'null';}
 					} else {
