0e31405d504405877f494d0153514994a421855d
Michael Levy <mike.levy75@gmail.com>
2016-04-22 19:24:05 -0400

    organization

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+require('es6-promise').polyfill();
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var uglify = require('gulp-uglify');
+var cssnano = require('gulp-cssnano');
+var runSequence = require('run-sequence');
+var rename = require('gulp-rename');
+var replace = require('gulp-replace');
+var fs = require('fs');
+
+var paths = {
+  js: 'js',
+  scss: 'scss',
+  buildJs: 'dist/js',
+  buildCss: 'dist/css'
+};
+
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: ''
+    },
+    startPath: 'demo.html'
+  })
+})
+
+gulp.task('sass', function() {
+  return gulp.src(paths.scss+'/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sass()) // Passes it through a gulp-sass
+    .pipe(gulp.dest(paths.buildCss)) // Outputs it in the css folder
+    .pipe(browserSync.reload({ // Reloading with Browser Sync
+      stream: true
+    }));
+})
+
+gulp.task('watch', function() {
+  gulp.watch(paths.scss+'/*.scss', ['sass']);
+  gulp.watch('demo.html', browserSync.reload);
+  gulp.watch(paths.js+'/*.js', ['js-style', 'js']);
+})
+
+gulp.task('js', function() {
+  return gulp.src(paths.buildJs+'/console.js')
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(paths.buildJs))
+    .pipe(browserSync.reload({ // Reloading with Browser Sync
+      stream: true
+    }));
+});
+
+gulp.task('css', function() {
+  return gulp.src(paths.buildCss+'/*.css')
+    .pipe(cssnano())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(paths.buildCss));
+});
+
+gulp.task('js-style', function() {
+  return gulp.src(paths.js+'/*.js')
+    .pipe(replace(/{{consoleLogStyles}}/, function(s) {
+        var style = fs.readFileSync(paths.buildCss+'/console-style.min.css', 'utf8');
+        return style;
+    }))
+    .pipe(gulp.dest(paths.buildJs));
+});
+
+gulp.task('default', function(callback) {
+  runSequence(['sass', 'browserSync', 'watch'], 'css', callback);
+});
